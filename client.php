2ea3da1809b90db3ccb15c67ea6da43e2ee0c09e
Ishita <ishitajaisia@gmail.com>
2016-12-24 23:18:23 +0530

    update in client.php
    client table added

--- client.php
+++ client.php
@@ -27,6 +27,36 @@ echo "<tr>
 </tr>"
 ?>
 </table>
+<table border='1' align='Center'>
+<thead>
+<th><strong>Hardware Type</strong></th>
+<th><strong>Model No</strong></th>
+<th><strong>Item No</strong></th>
+<th><strong>Quantity</strong></th>
+<th><strong>Date Of Allocation</strong></th>
+<th><strong>Period Of Allocation</strong></th>
+<th><strong>Warranty Date</strong></th>
+<th><strong></strong></th>
+</thead>
+<tbody>
+<?php
+$query1="select HW_Type,Model_No,Item_No,Quantity,Date_of_All,Period_of_All,Warranty_Date from employee_items where Employee_ID= '$var'";
+$run1=mysql_query($query1);
+while($row1=mysql_fetch_array($run1))
+{
+	echo "<tr>
+		<td>{$row['HW_Type']}</td>
+		<td>{$row['Model_No']}</td>
+		<td>{$row['Item_No']}</td>
+		<td>{$row['Quantity']}</td>
+		<td>{$row['Date_of_All']}</td>
+		<td>{$row['Period_of_All']}</td>
+		<td>{$row['Warranty_Date']}</td>
+		</tr>";
+}
+?>
+</tbody>
+</table>
 <?php
 echo '<p align="center"><a href="request.php">Request an Item</a><br>
 <a href="issue.php">Raise an Issue</a><br>
